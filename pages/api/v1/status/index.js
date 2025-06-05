8b6d28f017c2bded882daa5898c2917462cd8807
Julius Mergulhao <juliusczm@gmail.com>
2025-06-05 14:35:15 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,6 +1,28 @@
 import database from "infra/database.js"
 
 export default async function status(req, res) {
-  const result = await database.query('SELECT 1+1 as sum;')
-  res.status(200).json({chave: "curso maneiro"})
+  const updateAt = new Date().toISOString();
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+    });
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  res.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        //status: null,
+        max_connections: Number(dbMaxConnectionsValue),
+        opened_connections: dbOpenedConnectionsValue,
+        version: dbVersionValue,
+      }
+    }
+  });
 }
--- tests/integration/ap1/v1/status/get.test.js
+++ tests/integration/ap1/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return 200", async () =>{
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+
+  const {max_connections, opened_connections, version} = responseBody.dependencies.database;
+  expect(max_connections).toBe(100);
+  expect(opened_connections).toBe(1);
+  expect(version).toBe('16.9'); 
+
 })
