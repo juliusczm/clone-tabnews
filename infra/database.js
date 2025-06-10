a2176623e8df2751d84b253c12478e5a4da61f16
Julius Mergulhao <juliusczm@gmail.com>
2025-06-10 14:19:43 -0300

    adds `api/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,20 @@
-import { Client } from "pg"
+import { Client } from 'pg';
 
-async function query(queryObject){
+async function query(queryObject) {
+  let client;
+
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.end();
+  }
+}
+
+async function getNewClient() {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
@@ -9,24 +23,16 @@ async function query(queryObject){
     password: process.env.POSTGRES_PASSWORD,
     ssl: getSSLValues(),
   });
-
- 
-  try {
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error){
-    console.error(error)
-  }finally {
-    await client.end();
-  }
+  await client.connect();
+  return client;
 }
 
 export default {
-  query: query,
-}
+  query,
+  getNewClient,
+};
 
-function getSSLValues(){
+function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
