bd28a8af828fe356bac7f99bb831d1d19baddb1c
Julius Mergulhao <juliusczm@gmail.com>
2025-06-15 22:08:14 -0300

    adds `Linting` GitHub Action and fix some files format

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,17 +1,17 @@
-import database from "infra/database.js"
+import database from 'infra/database.js';
 
 export default async function status(req, res) {
   const updateAt = new Date().toISOString();
-  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionResult = await database.query('SHOW server_version;');
   const dbVersionValue = dbVersionResult.rows[0].server_version;
-  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsResult = await database.query('SHOW max_connections;');
 
   const dbName = process.env.POSTGRES_DB;
   const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
   const dbOpenedConnectionsResult = await database.query({
-    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    text: 'SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;',
     values: [dbName],
-    });
+  });
   const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
 
   res.status(200).json({
@@ -22,7 +22,7 @@ export default async function status(req, res) {
         max_connections: Number(dbMaxConnectionsValue),
         opened_connections: dbOpenedConnectionsValue,
         version: dbVersionValue,
-      }
-    }
+      },
+    },
   });
 }
