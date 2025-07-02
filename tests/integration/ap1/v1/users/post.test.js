fc46dab89f4b7479d8874cf80bda120b75ede1ea
Julius Mergulhao <juliusczm@gmail.com>
2025-07-01 21:36:52 -0300

    feat: hash `password` in `user.create`

--- tests/integration/ap1/v1/users/post.test.js
+++ tests/integration/ap1/v1/users/post.test.js
@@ -1,5 +1,7 @@
 import orchestrator from 'tests/orchestrator.js';
 import { version as uuidVersion } from 'uuid';
+import user from 'models/user.js';
+import password from 'models/password.js';
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -30,7 +32,7 @@ describe('POST /api/v1/users', () => {
         id: responseBody.id,
         username: 'juliusczm',
         email: 'juliusczm@gmail.com',
-        password: 'senha123',
+        password: responseBody.password,
         created_at: responseBody.created_at,
         updated_at: responseBody.updated_at,
       });
@@ -38,6 +40,12 @@ describe('POST /api/v1/users', () => {
       expect(uuidVersion(responseBody.id)).toBe(4);
       expect(Date.parse(responseBody.created_at)).not.toBeNaN();
       expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+      const userInDatabase = await user.findOneByUsername('juliusczm');
+      const correctPasswordMatch = await password.compare('senha123', userInDatabase.password);
+      const incorrectPasswordMatch = await password.compare('senha', userInDatabase.password);
+      expect(correctPasswordMatch).toBe(true);
+      expect(incorrectPasswordMatch).toBe(false);
     });
 
     test('with duplicated email', async () => {
